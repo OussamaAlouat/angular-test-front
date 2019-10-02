21c1b1ee18a0e1a89049bb7ad0be94392bd26202
oussamaAlouat <al317257@uji.es>
2019-10-02 18:36:12 +0200

    Checkbox tested

--- src/app/commons/checkbox/checkbox.component.spec.ts
+++ src/app/commons/checkbox/checkbox.component.spec.ts
@@ -5,21 +5,30 @@ import { CheckboxComponent } from './checkbox.component';
 describe('CheckboxComponent', () => {
   let component: CheckboxComponent;
   let fixture: ComponentFixture<CheckboxComponent>;
+  let element: HTMLElement;
+  let elementCheckbox: HTMLElement;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ CheckboxComponent ]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(CheckboxComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+    element = fixture.debugElement.nativeElement;
+    elementCheckbox = element.querySelector('input');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Click on a checkbox should emit a change event', () =>{
+    spyOn(component.changed, 'emit');
+    elementCheckbox.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    expect(component.changed.emit).toHaveBeenCalled();
   });
 });
--- src/app/commons/checkbox/checkbox.component.ts
+++ src/app/commons/checkbox/checkbox.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss']
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent {
   @Input()
   checked: boolean;
 
+  @Output()
+  changed: EventEmitter<any>;
+
   constructor() {
     this.checked = true;
+    this.changed = new EventEmitter();
   }
 
-  ngOnInit() {
+  handleChange(event: any) {
+    event.stopPropagation();
+    this.changed.emit({ checked: event.target.checked });
   }
 
 }
