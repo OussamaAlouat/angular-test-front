5dd23d2f087e10ac0f29e6e3683262794bc59989
oussamaAlouat <al317257@uji.es>
2019-10-02 18:53:42 +0200

    Generate module and generate todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonsModule } from './commons/commons.module';
+import { TodoComponent } from './todoList/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
