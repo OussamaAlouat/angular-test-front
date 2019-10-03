2b5136457b4e54ec0c5bdaf3367390e80e772aaf
oussamaAlouat <al317257@uji.es>
2019-10-03 17:29:41 +0200

    Fixed app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonsModule } from './commons/commons.module';
-import { TodoComponent } from './todo-list/todo/todo.component';
 import { TodoListModule } from './todo-list/todo-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonsModule,
     TodoListModule
   ],
   providers: [],
