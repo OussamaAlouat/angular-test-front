ad55a7a4156447ab016b099f2ccd9487fcb7d9d4
oussamaAlouat <al317257@uji.es>
2019-10-03 17:47:41 +0200

    remove unnecesary code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,7 +10,6 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        CommonsModule,
         TodoListModule
       ],
       declarations: [
